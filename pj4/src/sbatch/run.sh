e7438c36a288f79908c1e9062229f14c54fcf5b4
awesomejiang <awesomejiang1995@gmail.com>
2018-05-13 23:35:00 -0500

    fix bash scripts

--- pj4/src/sbatch/run.sh
+++ pj4/src/sbatch/run.sh
@@ -3,14 +3,6 @@
 perf="strong weak"
 num="1 2 4 8 16 32 64"
 
-module load openmpi/2.0.1+gcc-6.1
-
-cd src
-
-make
-
-cd sbatch
-
 for p in $perf
 do
 	for n in $num
@@ -18,7 +10,3 @@ do
 		sbatch $p\_$n.bat
 	done
 done
-
-cd ..
-
-make clean
