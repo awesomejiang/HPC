b7945408e5a20b4e8cfd41c6301f3aaa965ec1ab
awesomejiang <awesomejiang1995@gmail.com>
2018-05-24 16:04:47 -0500

    result

--- pj5/src/sbatch.bat
+++ pj5/src/sbatch.bat
@@ -8,18 +8,18 @@
 module load cuda/8.0
 module load gcc/4.8
 
-../main cuda 1000 4 250
-../main cuda 1000 40 250
-../main cuda 1000 400 250
-../main cuda 1000 4000 250
-../main cuda 1000 40000 250
-../main cuda 1000 400000 250
-../main cuda 1000 4000000 250
+./main cuda 1000 4 250
+./main cuda 1000 40 250
+./main cuda 1000 400 250
+./main cuda 1000 4000 250
+./main cuda 1000 40000 250
+./main cuda 1000 400000 250
+./main cuda 1000 4000000 250
 
-../main serial 1000 1000
-../main serial 1000 10000
-../main serial 1000 100000
-../main serial 1000 1000000
-../main serial 1000 10000000
-../main serial 1000 100000000
-../main serial 1000 1000000000
+./main serial 1000 1000
+./main serial 1000 10000
+./main serial 1000 100000
+./main serial 1000 1000000
+./main serial 1000 10000000
+./main serial 1000 100000000
+./main serial 1000 1000000000
